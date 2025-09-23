4b3f31601a036e4ec56e67019e5149ac38f35554
Lance Nathan <lancenathandebelen@gmail.com>
2025-09-23 20:02:35 +0800

    feat: ways to do format specifiers

--- 007_print.py
+++ 007_print.py
@@ -13,3 +13,13 @@ def ds():
     """
 
 print(ds.__doc__)   #prints the docstring
+
+# format specifier
+pi = 3.14159265
+print("Pi with 2 decimals: %.2f" % pi)   # 3.14
+print("Integer: %d, Hex: %x" % (255, 255))  # Integer: 255, Hex: ff
+
+# str.format()
+pi = 3.14159265
+print("Pi with 2 decimals: {:.2f}".format(pi))   # 3.14
+print("Binary of 10: {:b}".format(10))           # 1010
